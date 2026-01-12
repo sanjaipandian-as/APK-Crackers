059a37da9f40431c7a65831d709c3fd09ebb3ee5
Sanjaipandian-as <sanjaipandian.as@gmail.com>
2026-01-12 15:05:37 +0530

    Frontend updated

--- frontend/api.js
+++ frontend/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://apk-crackers.onrender.com/api",
+    baseURL: "https://apk-crackers-zaeo.onrender.com/api",
     headers: {
         "Content-Type": "application/json",
     },
